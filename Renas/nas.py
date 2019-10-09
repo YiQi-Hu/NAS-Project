51ae3533f510236e178ffe7bd3a9116372a80737
bluebread <hotbread70127@gmail.com>
2019-10-09 10:59:04 +0800

    fsb

--- Renas/nas.py
+++ Renas/nas.py
@@ -40,8 +40,8 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 NAS_CONFIG = json.load(open(NAS_CONFIG_PATH, encoding='utf-8'))
 
 # TODO Fatal: Corenas can not get items in IDLE_GPUQ (Queue)
-IDLE_GPUQ = [i for i in range(NAS_CONFIG['num_gpu'])]
-# IDLE_GPUQ = Queue()
+# IDLE_GPUQ = [i for i in range(NAS_CONFIG['num_gpu'])]
+IDLE_GPUQ = Queue()
 
 def _gpu_eva(params):
     graph, cell, nn_pb, _, p_, ft_sign, pl_, eva, ngpu = params
@@ -61,8 +61,8 @@ def _gpu_eva(params):
             except:
                 print(SYS_EVAFAIL)
                 f.write(LOG_EVAFAIL)
-        IDLE_GPUQ.append(ngpu)
-        # IDLE_GPUQ.put(ngpu)
+        # IDLE_GPUQ.append(ngpu)
+        IDLE_GPUQ.put(ngpu)
 
     end_time = time.time()
     start_time = time.time()
@@ -94,13 +94,13 @@ def _wait_for_event(event_func):
 def _do_task(pool, cmnct):
     result_list = []
     while not cmnct.task.empty():
-        gpu = IDLE_GPUQ.pop()
-        # gpu = IDLE_GPUQ.get()
+        # gpu = IDLE_GPUQ.pop()
+        gpu = IDLE_GPUQ.get()
         try:
             task_params = cmnct.task.get(timeout=1)
         except:
-            IDLE_GPUQ.append(gpu)
-            # IDLE_GPUQ.put(gpu)
+            # IDLE_GPUQ.append(gpu)
+            IDLE_GPUQ.put(gpu)
             break
         result = pool.apply_async(_gpu_eva, args=task_params)
         result_list.append(result)
