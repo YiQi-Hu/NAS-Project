2d529f23f670c181e003796860ed2daffa5c690d
nuaa-QK <1248443014@qq.com>
2019-11-08 19:26:30 +0800

    slove deepcopy

--- glo_corenas.py
+++ glo_corenas.py
@@ -4,7 +4,7 @@ from multiprocessing import Pool
 from numpy import zeros
 import nas
 import random
-import os
+import os,copy
 #from nas import _gpu_eva
 from base import Network ,NetworkItem
 def save_glolog(fp,Net,num):
@@ -24,12 +24,12 @@ def save_glolog(fp,Net,num):
 	fp.flush()
 	return 
 def run_global(eva,graph,cell,ngpu,gpu_list):
-	print(str(ngpu))
+	#print(str(ngpu))
 	score=random.random()
 	os.environ["CUDA_VISIBLE_DEVICES"] = str(ngpu);
 	#score=eva.evaluate(graph,cell,[])
 	#score=random.random();
-	print(str(ngpu)+"  achieve",cell,graph)
+	#print(str(ngpu)+"  achieve",cell,graph)
 	gpu_list.put(ngpu)
 	return score
 def initialize_ops(NETWORK_POOL,NetworkItem):
@@ -47,7 +47,7 @@ def initialize_ops(NETWORK_POOL,NetworkItem):
 def dis_global(enu,eva,num_gpu):
 	manager = multiprocessing.Manager()
 	gpu_list = manager.Queue()
-	f1=open("1.txt",'a')
+	os.remove('1.txt');f1=open("1.txt",'a')
 	NETWORK_POOL =enu.enumerate()
 	block_num=0
 	for network in NETWORK_POOL:
@@ -59,22 +59,23 @@ def dis_global(enu,eva,num_gpu):
 	Net=Network(0,nn.graph_part);
 	for gpu in range(0,num_gpu):     #not test first_samper
 		gpu_list.put(gpu)
-		Net_item.cell ,Net_item.graph, Net_item.code =nn.spl.sample()
-		Net.item_list.append(Net_item)
+		Net_item.cell ,Net_item.graph, Net_item.code =nn.spl.sample();tmp=copy.deepcopy(Net_item)
+		Net.item_list.append(tmp)
 	pool= Pool(processes=num_gpu);eva.add_data(300);
 	results=[];i=0
 	while i<200:
 		while not gpu_list.empty():
 			ngpu=gpu_list.get();
 			eva_result=pool.apply_async(run_global,args=(eva,Net.item_list[i].graph,Net.item_list[i].cell,ngpu,gpu_list,))
-			print(eva_result,i,ngpu,Net.item_list[i].graph,Net.item_list[i].cell,flush=True);
+			#print(eva_result,i,ngpu,Net.item_list[i].graph,Net.item_list[i].cell,flush=True);
 			results.append(eva_result);i=i+1;
 		k=0
 		for result in results[i-2:]:
-			Net.item_list[i-2+k].score=result.get();#print("score",score)
+			Net.item_list[i-2+k].score=result.get();
+			print(eva_result,i-2+k,Net.item_list[i-2+k].score,Net.item_list[i-2+k].cell,Net.item_list[i-2+k].graph,flush=True);
 			nn.spl.update_opt_model(Net.item_list[i-2+k].code,Net.item_list[i-2+k].score)
-			Net_item.cell ,Net_item.graph, Net_item.code =nn.spl.sample()
-			Net.item_list.append(Net_item)
+			Net_item.cell ,Net_item.graph, Net_item.code =nn.spl.sample();tmp=copy.deepcopy(Net_item)
+			Net.item_list.append(tmp)
 			save_glolog(f1,Net,i-2+k)
 			k=k+1;
 	pool.close()#
