8ca83e0b24945e52dafcee4ae7bd7b06626bebb8
pjsss <35166356+pjsss@users.noreply.github.com>
2019-11-26 13:41:21 +0800

    add sep_conv

--- base.py
+++ base.py
@@ -4,7 +4,6 @@ PLEASE DO NOT USE 'from .base import *' !!!
 """
 
 
-from multiprocessing import Pool
 class Network(object):
     pre_block = []
 
@@ -12,15 +11,22 @@ class Network(object):
         self.id = _id
         self.graph_template = graph_tmp
         self.item_list = []
+        self.pre_block = []
         self.spl = None
 
+        return
+
+
 class NetworkItem(object):
-    def __init__(self, _id=0, graph=[], cell_list=[], code=[]):
+    def __init__(self, _id, graph, cell_list, code):
         self.id = _id
         self.graph = graph
         self.cell_list = cell_list
         self.code = code
         self.score = 0
+
+        return
+
 
 class Cell(tuple):
     """class Cell inheirt from tuple
@@ -48,22 +54,11 @@ class Cell(tuple):
 
     def __init__(self, *args):
         tuple.__init__(self)
-
+    
     def __new__(self, *args):
-        if len(args) == 1 and isinstance(args[0], tuple):
-            args = args[0]
         Cell._check_vaild(args)
         return tuple.__new__(self, args)
-
-    def __getnewargs__(self):
-        return tuple.__getnewargs__(self)[0]
-
-    def __getstate__(self):
-        return [i for i in self]
     
-    def __setstate__(self, state):
-        self = Cell(*state)
-
     def __getattr__(self, key):
         """Get items though meaningful name
         if key is 'type':
@@ -90,13 +85,56 @@ class Cell(tuple):
             Cell._conv_vaild(type_args)
         elif cell_type == 'pooling':
             Cell._pool_valid(type_args)
+        elif cell_type == 'sep_conv':
+            Cell._sep_conv_valid(type_args)
         else:
             raise CellInitError('type error')
         return
-
+    
     @staticmethod
     def _conv_vaild(args):
         err_msg = 'cell type \'conv\' %s.'
+
+        if (len(args) > 3):
+            raise CellInitError(err_msg % 'args num > 3')
+        # fileter_size, kernel_size, activation
+        fs, ks, at = args
+        # '_tv' -> 'type valid'
+        fs_tv = isinstance(fs, int)
+        ks_tv = isinstance(ks, int)
+        at_tv = isinstance(at, str)
+
+        Cell._check_condition(fs_tv, ks_tv, at_tv, err_msg % 'arg type invalid') 
+        # '_rv' -> 'range valid'
+        fs_rv = (fs in range(1, 1025))
+        ks_rv = (ks % 2 == 1) and (ks in range(1, 10))
+        at_rv = (at in ['relu', 'tanh', 'sigmoid', 'identity', 'leakyrelu'])
+
+        Cell._check_condition(fs_rv, ks_rv, at_rv, err_msg % 'arg type invalid') 
+        return
+    
+    @staticmethod
+    def _pool_valid(args):
+        err_msg = 'cell type \'pooling\' %s'
+        if (len(args) > 2):
+            return CellInitError(err_msg % 'args num > 2')
+        # ptype, kernel_size
+        pt, ks = args 
+        # '_tv' -> 'type valid'
+        pt_tv = isinstance(pt, str)
+        ks_tv = isinstance(ks, int)
+
+        Cell._check_condition(pt_tv, ks_tv, err_msg % 'arg type invalid')
+        # '_rv' -> 'range valid'
+        pt_rv = (pt in ['avg', 'max', 'global'])
+        ks_rv = (ks in range(1, 11))
+
+        Cell._check_condition(pt_rv, ks_rv, err_msg % 'arg range invalid')
+        return
+
+    @staticmethod
+    def _sep_conv_valid(args):
+        err_msg = 'cell type \'sep_conv\' %s.'
 
         if (len(args) > 3):
             raise CellInitError(err_msg % 'args num > 3')
@@ -111,28 +149,9 @@ class Cell(tuple):
         # '_rv' -> 'range valid'
         fs_rv = (fs in range(1, 1025))
         ks_rv = (ks % 2 == 1) and (ks in range(1, 10))
-        at_rv = (at in ['relu', 'tanh', 'sigmoid', 'identity', 'leakyrelu', 'relu6'])
+        at_rv = (at in ['relu', 'tanh', 'sigmoid', 'identity', 'leakyrelu'])
 
         Cell._check_condition(fs_rv, ks_rv, at_rv, err_msg % 'arg type invalid')
-        return
-
-    @staticmethod
-    def _pool_valid(args):
-        err_msg = 'cell type \'pooling\' %s'
-        if (len(args) > 2):
-            return CellInitError(err_msg % 'args num > 2')
-        # ptype, kernel_size
-        pt, ks = args
-        # '_tv' -> 'type valid'
-        pt_tv = isinstance(pt, str)
-        ks_tv = isinstance(ks, int)
-
-        Cell._check_condition(pt_tv, ks_tv, err_msg % 'arg type invalid')
-        # '_rv' -> 'range valid'
-        pt_rv = (pt in ['avg', 'max', 'global'])
-        ks_rv = (ks in range(1, 11))
-
-        Cell._check_condition(pt_rv, ks_rv, err_msg % 'arg range invalid')
         return
 
     @staticmethod
@@ -147,35 +166,12 @@ class CellInitError(Exception):
     def __init__(self, msg):
         Exception.__init__(self, msg)
 
-def test_cell(item):
-    print("子进程", item.cell_list, item.graph,type(item.cell_list[0]))
-
 
 if __name__ == "__main__":
-    from base import Network, NetworkItem, Cell
-    #初始化一个Network
-    Net = Network(0, [[1], [2], [3], []])
+    cc = Cell('conv', 1024, 7, 'relu') # conv cell
+    pc = Cell('pooling', 'avg', 10) # pooling cell
 
-
-    cellist = [('conv', 512, 5, 'relu'), ('pooling',
-                                        'max', 3), ('pooling', 'max', 2), ]
-    cell_list = []
-    for x in cellist:
-        if len(x) == 4:
-            cell_list.append(Cell(x[0], x[1], x[2], x[3]))
-        else:
-            cell_list.append(Cell(x[0], x[1], x[2]))
-    #初始化一个NetworkItem
-    item = NetworkItem(0, [[1], [2], [3], []], cell_list, "")
-    print(type(cell_list))
-    Net.item_list.append(item)
-
-    print(Net.item_list[0])
-    print("主进程", Net.item_list[0].cell_list)
-    #测试子进程的cell_list
-
-
-    pool = Pool(2)
-    result = pool.apply_async(test_cell, args=(Net.item_list[0],))
-    pool.close()
-    pool.join()
+    print(cc.kernel_size)
+    print(pc.type)
+    print(cc)
+    print(pc)
