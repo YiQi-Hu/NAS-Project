fb5b8a2a5b9f14a30d5e96c266900c0e7808d74f
pjsss <35166356+pjsss@users.noreply.github.com>
2019-12-13 09:00:41 +0800

    Add files via upload

---
+++ base.py
@@ -0,0 +1,106 @@
+"""
+All basic data structure in this file.
+PLEASE DO NOT USE 'from .base import *' !!!
+"""
+from info_str import NAS_CONFIG
+
+from multiprocessing import Pool
+class Network(object):
+    pre_block = []
+
+    def __init__(self, _id, graph_tmp):
+        self.id = _id
+        self.graph_template = graph_tmp
+        self.item_list = []
+        self.spl = None
+
+class NetworkItem(object):
+    def __init__(self, _id=0, graph=[], cell_list=[], code=[]):
+        self.id = _id
+        self.graph = graph
+        self.cell_list = cell_list
+        self.code = code
+        self.score = 0
+
+class Cell(tuple):
+    """class Cell inheirt from tuple
+    Attributes:
+        1. cell_type (string, 'conv' or 'pooling')
+    Example:
+        conv_cell = Cell('conv', 48, 7, 'relu')
+        pooling_cell = Cell('pooling', 'avg', 9)
+        print(conv_cell.type) # 'conv'
+        print(pooling_cell.type) # 'pooling'
+        print(conv_cell) # ('conv', 48, 7, 'relu')
+        print(pooling_cell) # ('pooling', 'avg', 9)
+    """
+    __keys_index = {}
+    __SPACE_CONFIG = NAS_CONFIG['spl']['space']
+    for key in __SPACE_CONFIG.keys():
+        __keys_index[key] = tuple(__SPACE_CONFIG[key].keys())
+    
+    @classmethod
+    def get_format(cls, cell_type):
+        """Get keys order of cell according to cell's type.
+        Args:
+            cell_type: string, type of cell defined in configuration
+        Returns:
+            format: tuple, keys order
+        Example:
+            NAS_CONFIG['space'] = {
+                "conv": {
+                    "filter_size": [[...], ...],
+                    "kernel_size": [...],
+                    activation:[...]
+                },
+                "pooling":{
+                    ...
+                }, ...
+            }
+            Cell.get_format("conv") 
+            // Maybe return ("kernel_size", "activation", "filter_size")
+            conv_cell = Cell("conv", 3, "relu", 48)
+        """
+        return cls.__keys_index[cell_type]
+
+    def __init__(self, *args):
+        tuple.__init__(self)
+
+    def __new__(self, *args):
+        if len(args) == 1 and isinstance(args[0], tuple):
+            args = args[0]
+        return tuple.__new__(self, args)
+
+    def __getnewargs__(self):
+        return tuple.__getnewargs__(self)[0]
+
+    def __getstate__(self):
+        return [i for i in self]
+    
+    def __setstate__(self, state):
+        self = Cell(*state)
+
+    def __getattr__(self, key):
+        """Get items though meaningful name
+        if key is 'type', get the type of the cell.
+        otherwise, get the value according to the type and the search space.
+            
+        """
+        cell_type = self.__getitem__(0)
+        if key == 'type':
+            return cell_type
+        key_i = self.__keys_index[cell_type].index(key) + 1
+        return self.__getitem__(key_i)
+
+if __name__ == "__main__":
+    conv_space = NAS_CONFIG['spl']['space']['conv']
+    cfmt = Cell.get_format('conv')
+    tmp_list = ['conv']
+    for ks_v in cfmt:
+        ks = conv_space[ks_v]
+        if isinstance(ks[0], list):
+            tmp_list.append(ks[0][0])
+        else:
+            tmp_list.append(ks[0])
+    cell = Cell(*tmp_list)
+    print(cell)
--- sampler.py
+++ sampler.py
@@ -5,8 +5,6 @@ import copy
 from queue import Queue
 from base import Cell
 from info_str import NAS_CONFIG
-import collections
-import operator
 
 class Sampler:
 
@@ -31,15 +29,22 @@ class Sampler:
         self._graph_part_invisible_node = self.graph_part_add_invisible_node()
         self._crosslayer = self._get_crosslayer()
         # Read parameter table to get operation dictionary in stage(block_id)
-        self._setting = self._init_setting(block_id)
-
+        self._setting = dict()
+        self._setting['conv'] = copy.deepcopy(NAS_CONFIG['spl']['conv_space'])
+        self._setting['pooling'] = copy.deepcopy(NAS_CONFIG['spl']['pool_space'])
+        self._setting['sep_conv'] = copy.deepcopy(NAS_CONFIG['spl']['sep_conv_space'])
+        if self._pattern == "Block":
+            self._setting['conv']['filter_size'] = \
+                self._setting['conv']['filter_size'][block_id]
+            self._setting['sep_conv']['filter_size'] = \
+                self._setting['sep_conv']['filter_size'][block_id]
         self._dic_index = self._init_dict()  # check
         # Set parameters of optimization module based on the above results
         self.__region, self.__type = self._opt_parameters()
         self.__dim = Dimension()
         self.__dim.set_dimension_size(len(self.__region))    # 10%
         self.__dim.set_regions(self.__region, self.__type)
-        self.__parameters_subscript = []
+        self.__parameters_subscript = []  #
         self.opt = Optimizer(self.__dim, self.__parameters_subscript)
         opt_para = copy.deepcopy(NAS_CONFIG["opt"])
         __sample_size = opt_para["sample_size"]  # the instance number of sampling in an iteration
@@ -75,22 +80,7 @@ class Sampler:
             2. score（float, 0 ~ 1.0)
         No returns.
         """
-        self.opt.update_model(table, score)  # here "-" represent that we minimize the loss
-
-    def _init_setting(self, block_id):
-        _setting_tmp = collections.OrderedDict()
-        _setting_tmp = copy.deepcopy(NAS_CONFIG['spl']['space'])
-
-        for key in _setting_tmp:
-            for op in _setting_tmp[key]:
-                if type(_setting_tmp[key][op][0]) is list:
-                    _setting_tmp[key][op] = _setting_tmp[key][op][block_id]
-
-        #TODO del(pjs)  del pooling in config
-        if self._pattern == "Block":
-            del _setting_tmp['pooling']
-
-        return _setting_tmp
+        self.opt.update_model(table, score)
 
     def graph_part_add_invisible_node(self):
         graph_part_tmp = []
@@ -180,7 +170,6 @@ class Sampler:
             l = r
             r = l + len(self._dic_index) + self._cross_node_number
             p_node = self._p_table[l:r]  # Take the search space of a node
-            # print(p_node)
             node_cross_tmp = list(set(copy.deepcopy(p_node[len(self._dic_index):])))
             for i in node_cross_tmp:
                 if i != 0:
@@ -188,30 +177,37 @@ class Sampler:
             if not graph_part_sample[num]:
                 graph_part_sample[num].append(self._node_number)
 
-            for cnt, key_type in enumerate(self._setting):
-                if p_node[self._dic_index['type'][-1]] == cnt:
-                    tmp = (key_type,)
-                    #TODO Cell. X
-                    item_list = Cell.cell_tmp(key_type)
-
-                    for key_item in item_list:
-                        tmp = tmp + (self._setting[key_type][key_item]
-                                     [p_node[self._dic_index[key_type + ' ' + key_item][-1]]],)
-            # print(tmp)
-            tmp = Cell(tmp)
+            first = p_node[self._dic_index['type'][-1]]
+            tmp = ()
+            if self._pattern == "Block" and first == 1:
+                first = 0
+            if first == 0 or first == 2:  # Search operation under conv
+                if first == 0:
+                    conv_type = 'conv'
+                else:
+                    conv_type = 'sep_conv'
+                tmp = tmp + (conv_type,)
+                space_conv = ['filter_size', 'kernel_size', 'activation']
+                for key in space_conv:
+                    tmp = tmp + (self._setting[conv_type][key][p_node[self._dic_index[conv_type + ' ' + key][-1]]],)
+                tmp = Cell(tmp[0], tmp[1], tmp[2], tmp[3])
+            else:  # Search operation under pooling
+                tmp = tmp + ('pooling',)
+                space_pool = ['pooling_type', 'kernel_size']
+                for key in space_pool:
+                # for key in self._setting['pooling']:
+                    tmp = tmp + (self._setting['pooling'][key][p_node[self._dic_index['pooling ' + key][-1]]],)
+                tmp = Cell(tmp[0], tmp[1], tmp[2])
             res.append(tmp)
-
         return res, graph_part_sample
 
     def _init_dict(self):
         """Operation space dictionary based on parameter file."""
-
         dic = {}
         dic['type'] = (0, len(self._setting)-1, 0)
         cnt = 1
         num = 1
         for key in self._setting:
-
             for k in self._setting[key]:
                 tmp = len(self._setting[key][k]) - 1
                 dic[key + ' ' + k] = (cnt, cnt + tmp, num)
@@ -240,21 +236,13 @@ class Sampler:
         table = []
         l = 0
         r = 0
-        conv_cnt = 0
-        pooling_cnt = 1
-        for i, j in enumerate(self._setting):
-            if j == 'conv':
-                conv_cnt = i
-            if j == 'pooling':
-                pooling_cnt = i
         for num in range(self._node_number):  # Take the search space of a node
             l = r
             r = l + len(self._dic_index) + self._cross_node_number
             p_node = self._p_table[l:r]  # Sample value of the current node
-
             if len(ops[num]) != 1:
                 p_node = self._p_table[l:r]  # Sample value of the current node
-                p_node[self._dic_index['type'][-1]] = conv_cnt
+                p_node[self._dic_index['type'][-1]] = 0
                 for j, i in enumerate(self._setting['conv']['filter_size']):
                     if str(i) == ops[num][0]:
                         p_node[self._dic_index['conv filter_size'][-1]] = j
@@ -267,7 +255,7 @@ class Sampler:
                 table = table + p_node
             else:
                 if self._pattern == "Global":
-                    p_node[self._dic_index['type'][-1]] = pooling_cnt
+                    p_node[self._dic_index['type'][-1]] = 1
                 table = table + p_node
         return table
 
